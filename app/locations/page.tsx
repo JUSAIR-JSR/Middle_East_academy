5e2b6156d4bd73eb9b8262eb9ea87b7e08a317da
Jusair <jusairjsr123@gmail.com>
2025-05-23 20:16:06 +0530

    updated

---
+++ app/locations/page.tsx
@@ -0,0 +1,4 @@
+// app/locations/page.tsx
+export default function LocationsPage() {
+  return <h1>Locations</h1>;
+}
---
+++ app/support/page.tsx
@@ -0,0 +1,4 @@
+// app/locations/page.tsx
+export default function SupportPage() {
+  return <h1>Support</h1>;
+}
